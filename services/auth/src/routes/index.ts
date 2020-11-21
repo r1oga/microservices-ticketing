525f91b38cdba694d51f6dc638654540b463f685
r1oga <r1oga@tuta.io>
2020-11-21 21:14:48 +0100

    Restructure folders

--- services/auth/src/routes/index.ts
+++ services/auth/src/routes/index.ts
@@ -3,7 +3,7 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/api/users/currentUser', (_, res) =>
-  res.status(200).send('Hi There')
+  res.status(200).send('Hello world')
 )
 
 export { router }
