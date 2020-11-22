22bad461d55d41de2c4f2e4a023d90fa5b5c16fb
r1oga <r1oga@tuta.io>
2020-11-22 22:38:27 +0100

    refactor: Use current-user middleware

--- services/auth/src/middlewares/index.ts
+++ services/auth/src/middlewares/index.ts
@@ -9,3 +9,4 @@ const middlewares = [
 export { middlewares }
 export { errorHandler } from './error-handler'
 export { validateRequest } from './validate-request'
+export { currentUser } from './current-user'
