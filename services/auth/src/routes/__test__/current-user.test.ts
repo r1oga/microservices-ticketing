ee42e94ee82035bc288bf6bb86829b519c4faabb
r1oga <r1oga@tuta.io>
2020-11-23 23:01:11 +0100

    refactor: Define global signup helper funtion

--- services/auth/src/routes/__test__/current-user.test.ts
+++ services/auth/src/routes/__test__/current-user.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('responds with details about current user', async () => {
+  /*
   const signupResponse = await request(app)
     .post('/api/users/signup')
     .send({ email: 'test@test.co', password: 'password' })
@@ -12,21 +13,32 @@ it('responds with details about current user', async () => {
     .send()
     .expect(200)
 
-  /*
-    won't see the cookie. (currentUser: null)
-    cookies are managed by browsers.
-    supertest doesn't manage cookies automatically.
-    cookie isn't sent along with second request.
-    need to include it manually
-  */
+  
+    // won't see the cookie. (currentUser: null)
+    // cookies are managed by browsers.
+    // supertest doesn't manage cookies automatically.
+    // cookie isn't sent along with second request.
+    // need to include it manually
+  
   expect(responseWithoutCookie.body.currentUser === null)
 
-  const cookie = signupResponse.get('Set-Cookie')
-  const responseWithCookie = await request(app)
+  */
+
+  const cookie = await global.signup()
+  const response = await request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .send()
     .expect(200)
 
-  expect(responseWithCookie.body.currentUser.email).toEqual('test@test.co')
+  expect(response.body.currentUser.email).toEqual('test@test.com')
+})
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200)
+
+  expect(response.body.currentUser).toEqual(null)
 })
--- services/auth/src/test/setup.ts
+++ services/auth/src/test/setup.ts
@@ -1,7 +1,20 @@
+import request from 'supertest'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 
 import { app } from '../app'
+
+/*
+  to tell TypeScript about global signup function,
+  augment type definition
+*/
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup(): Promise<string[]>
+    }
+  }
+}
 
 let mongo: any
 beforeAll(async () => {
@@ -29,3 +42,20 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+/*
+  globally scoped function for easier use
+  only available in test env
+*/
+global.signup = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+  return cookie
+}
