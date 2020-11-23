2ff03e29d51c222f3f70a30c8d3aee7393201d21
r1oga <r1oga@tuta.io>
2020-11-23 22:12:43 +0100

    test: Return 400 if email and/or pwd invalid

--- services/auth/src/routes/__test__/signup.test.ts
+++ services/auth/src/routes/__test__/signup.test.ts
@@ -6,3 +6,34 @@ it('returns a 201 on successful signup', () =>
     .post('/api/users/signup')
     .send({ email: 'test@test.co', password: 'password' })
     .expect(201))
+
+it('returns a 400 if email invalid', () =>
+  request(app)
+    .post('/api/users/signup')
+    .send({ email: 'testtest.co', password: 'password' })
+    .expect(400))
+
+it('returns a 400 if password not between 4 and 20 characters long', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.co', password: 'pwd' })
+    .expect(400)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.co', password: 'passssssssssssssssssss' })
+    .expect(400)
+})
+
+it('returns a 400 if password or email are missing', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.co', password: '' })
+    .expect(400)
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: '', password: 'password' })
+    .expect(400)
+})
+it('', () => {})
