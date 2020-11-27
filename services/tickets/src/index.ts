12373b5e2a70748cef0f6dd292cf69b244023b92
r1oga <r1oga@tuta.io>
2020-11-27 17:16:15 +0100

    refactor: Use env vars to define mongo URI

--- services/tickets/src/index.ts
+++ services/tickets/src/index.ts
@@ -12,7 +12,11 @@ const start = async () => {
       throw new Error('JWT_KEY not defined')
     }
 
-    await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets', {
+    if (!process.env.MONGO_URI) {
+      throw new Error('MONGO_URI not defined')
+    }
+
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
