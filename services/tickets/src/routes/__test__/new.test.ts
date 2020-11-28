dbf9bf8cd6ba090a4853328aab60e56d5ca2fc68
r1oga <r1oga@tuta.io>
2020-11-28 13:31:44 +0100

    test: Validate title and price to create ticket

--- services/tickets/src/routes/__test__/new.test.ts
+++ services/tickets/src/routes/__test__/new.test.ts
@@ -16,3 +16,21 @@ it('can only be accessed if user is signed in', async () => {
     .send({ title: 'a', price: 10 })
     .expect(200)
 })
+
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({ title: '', price: 10 })
+    .expect(400)
+})
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({ title: 'Test', price: 'invalid' })
+    .expect(400)
+})
+
+it('creates a ticket if valid inputs are provided', async () => {})
