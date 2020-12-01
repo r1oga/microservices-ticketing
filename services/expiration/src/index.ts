a1b4faca9cf5d713e5060b3cde0dba3bbb23b63d
r1oga <r1oga@tuta.io>
2020-12-01 16:53:41 +0100

    Skaffold expiration service

---
+++ services/expiration/src/index.ts
@@ -0,0 +1,33 @@
+import { natsWrapper } from './nats-wrapper'
+
+const start = async () => {
+  /*
+    type guard for env variables
+    in start function rather than route file
+    so that error is caught right at app start
+  */
+  const { NATS_CLUSTER_ID, NATS_URL, NATS_CLIENT_ID } = process.env
+  if (!NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID not defined')
+  if (!NATS_URL) throw new Error('NATS_URL not defined')
+  if (!NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID not defined')
+
+  try {
+    await natsWrapper.connect(NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL)
+
+    /*
+      handle connection close
+      as we call process.exit(), better not to hide it in
+      an other file and make it visible in index entry file
+    */
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed')
+      process.exit()
+    })
+    process.on('SIGINT', () => natsWrapper.client.close()) // interrupt signal
+    process.on('SIGTERM', () => natsWrapper.client.close()) // terminate signal (ctrl+c)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+start()
