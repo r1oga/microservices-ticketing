55b8566b00d90d2a1e0d93f1f0e95fc2820032d9
r1oga <r1oga@tuta.io>
2020-12-02 22:22:53 +0100

    feat: Display expiration timer

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,10 @@
 import { Order } from '../../components'
 
-const OrderShow = () => <Order />
+const OrderShow = ({ order }) => <Order {...order} />
 
+OrderShow.getInitialProps = async (context, axios) => {
+  const { orderId } = context.query
+  const { data: order } = await axios.get(`/api/orders/${orderId}`)
+  return { order }
+}
 export default OrderShow
